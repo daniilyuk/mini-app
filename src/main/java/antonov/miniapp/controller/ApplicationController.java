8f08ae3c431052dbe6c380edf854f6ace86b217e
daniilyuk <daniilantonov406@gmail.com>
2025-04-30 00:54:13 +0300

    init

---
+++ src/main/java/antonov/miniapp/controller/ApplicationController.java
@@ -0,0 +1,13 @@
+package antonov.miniapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
