b416bce05b48e38eb7f941de6513bcab7f84ddc0
daniilyuk <daniilantonov406@gmail.com>
2025-04-30 02:12:19 +0300

    add logger

--- src/main/java/antonov/miniapp/controller/ApplicationController.java
+++ src/main/java/antonov/miniapp/controller/ApplicationController.java
@@ -1,13 +1,19 @@
 package antonov.miniapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApplicationController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationController.class);
+
     @GetMapping("/hello")
     public String sayHello() {
+        logger.info("Hello!");
         return "Hello, World!";
     }
 }
